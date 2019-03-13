a073830ce7d026e9b8bd45e3c66d0241ff43bc4f
lobray <leslieobray@gmail.com>
2019-03-13 11:11:55 +0100

    functioning yaml file and search tweets!

--- search_tweets.py
+++ search_tweets.py
@@ -3,42 +3,47 @@ import numpy as np
 from searchtweets import ResultStream, gen_rule_payload, load_credentials
 from searchtweets import collect_results
 
-
 import twitter_credentials
 
-
 premium_search_args = load_credentials("~/.twitter_keys.yaml",
-                                       yaml_key="search_tweets_premium",
-                                       env_overwrite=False)
+                                       account_type="premium")
 
 
-rule = gen_rule_payload("from:jack",
-                        from_date="2017-09-01", #UTC 2017-09-01 00:00
-                        to_date="2017-10-30",#UTC 2017-10-30 00:00
+
+rule = gen_rule_payload("selbstbestimmungsinitiative",
+                        from_date="2018-09-01", #UTC 2017-09-01 00:00
+                        to_date="2018-10-30",#UTC 2017-10-30 00:00
                         results_per_call=100) #for sandbox. can also add "count bucket"
-print(rule)
 
 print(rule)
 
-tweets = collect_results(rule,
-                         max_results=100,
-                         result_stream_args=premium_search_args) # change this if you need to - is this only for counts
-
-[print(tweet.created_at_datetime) for tweet in tweets[0:10]]
-[print(tweet.generator.get("name")) for tweet in tweets[0:10]]
-# HOW DOES THIS WORK WTIH PAGINATION?
+# tweets = collect_results(rule,
+#                          max_results=100,
+#                          result_stream_args=premium_search_args) # change this if you need to - is this only for counts
+# aaaaaaaaaaaaaaaaaa
+# for tweet in tweets[0:10]:
+#   print(tweet.all_text) 
+# [print(tweet.generator.get("name")) for tweet in tweets[0:10]]
+# # HOW DOES THIS WORK WTIH PAGINATION?
 
 rs = ResultStream(rule_payload=rule,
                   max_results=100, # for sandbox
                   max_pages=1,
+                  # endpoint = "https://api.twitter.com/1.1/tweets/search/fullarchive/dev.json",
                   **premium_search_args)
 
 print(rs)
 
+print(type(rs))
+print(dir(rs))
+
+
 tweets = list(rs.stream())
+print(len(tweets))
 
 # using unidecode to prevent emoji/accents printing
-[print(tweet.all_text) for tweet in tweets[0:10]];
+for tweet in tweets[0:100]:
+  print(tweet.all_text) 
 
 
 
